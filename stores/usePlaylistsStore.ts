5739df08b81661719201d5ae45d2bfa0fe188cca
sibvan <sibvan@icloud.com>
2026-01-15 02:15:29 +0400

    add store

---
+++ stores/usePlaylistsStore.ts
@@ -0,0 +1,70 @@
+import { create } from "zustand";
+
+type Video = {
+  id: string;
+  theory: boolean;
+  practice: boolean;
+  title: string;
+};
+
+type VideoFeature = "theory" | "practice";
+
+type Playlist = {
+  id: string;
+  title: string;
+  url: string;
+  videos: Video[];
+};
+
+type PlaylistsStore = {
+  playlists: Playlist[];
+  getPlaylistNum: () => number;
+
+  setPlaylists: (playlists: Playlist[]) => void;
+  addPlaylist: (playlist: Playlist) => void;
+  findPlaylistById: (id: string) => Playlist | undefined;
+  changeStatus: (
+    playlistId: string,
+    videoId: string,
+    feature: VideoFeature,
+  ) => void;
+};
+
+export const usePlaylistsStore = create<PlaylistsStore>((set, get) => ({
+  playlists: [],
+
+  getPlaylistNum: () => {
+    return get().playlists.length;
+  },
+
+  setPlaylists: (playlists) => {
+    set({ playlists });
+  },
+
+  findPlaylistById: (id: string) =>
+    get().playlists.find((playlist) => playlist.id === id),
+
+  changeStatus: (playlistId, videoId, feature) => {
+    set((state) => ({
+      playlists: state.playlists.map((playlist) =>
+        playlist.id !== playlistId
+          ? playlist
+          : {
+              ...playlist,
+              videos: playlist.videos.map((video) =>
+                video.id === videoId
+                  ? { ...video, [feature]: !video[feature] }
+                  : video,
+              ),
+            },
+      ),
+    }));
+  },
+
+  addPlaylist: (playlist) => {
+    const exists = get().playlists.some((p) => p.id === playlist.id);
+    if (!exists) {
+      set((state) => ({ playlists: [...state.playlists, playlist] }));
+    }
+  },
+}));
