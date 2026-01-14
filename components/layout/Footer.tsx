9fbed8057a3e825ca99ae39e26ba59cf3a88d11e
sibvan <sibvan@icloud.com>
2026-01-15 03:38:02 +0400

    add updates page

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,24 +1,32 @@
 import Logo from "../ui/Logo";
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <footer className="mt-auto">
       <div className="mb-4 h-px w-full bg-black"></div>
       <div className="flex justify-between">
-        <p className="font-title text-[16px]/[100%] font-normal">
-          ©
-          <Logo />, 2025
+        <p className="font-title flex flex-col text-[16px]/[100%] font-normal md:flex-row">
+          © 
+          <Logo />
+          <span className="hidden md:block">, </span>
+          2025
         </p>
-        <p className="text-16">
-          Created by 
-          <a
-            className="decoration-red underline"
-            target="_blank"
-            href="https://sibvan.dev/"
-          >
-            sibvan.dev
-          </a>
-        </p>
+        <div className="flex flex-col gap-4 md:flex-row">
+          <Link className="text-16" href="/updates">
+            Обновления
+          </Link>
+          <p className="text-16">
+            Created by 
+            <a
+              className="decoration-red underline"
+              target="_blank"
+              href="https://sibvan.dev/"
+            >
+              sibvan.dev
+            </a>
+          </p>
+        </div>
       </div>
     </footer>
   );
