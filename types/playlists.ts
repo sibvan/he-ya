594e8f1f3afe36c17f8ed057f9485109782035f4
sibvan <sibvan@icloud.com>
2026-02-08 15:02:46 +0400

    add ts types

---
+++ types/playlists.ts
@@ -0,0 +1,38 @@
+type PlaylistsStore = {
+  playlists: Playlist[];
+  setPlaylists: (playlists: Playlist[]) => void;
+  addPlaylist: (playlist: Playlist) => void;
+  deletePlaylist: (playlist: Playlist) => void;
+  changeStatus: (
+    playlist: Playlist,
+    videoId: string,
+    type: TheoryOrPractice,
+  ) => void;
+};
+
+type TheoryOrPractice = "theory" | "practice";
+
+type Video = {
+  id: string;
+  link: string;
+  practice: boolean;
+  theory: boolean;
+  title: string;
+};
+
+type Playlist = {
+  playlistId: string;
+  userId: string;
+  title: string;
+  videos: Video[];
+};
+
+type YoutubeVideo = {
+  snippet: {
+    title: string;
+    resourceId: { videoId: string };
+  };
+  id: string;
+};
+
+export type { Playlist, YoutubeVideo, PlaylistsStore, Video, TheoryOrPractice };
