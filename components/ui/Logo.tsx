ba51f433b7b88307f46051d8ce5614e5debeaf5a
sibvan <sibvan@icloud.com>
2026-02-08 22:54:56 +0400

    add logo component

---
+++ components/ui/Logo.tsx
@@ -0,0 +1,11 @@
+const Logo = () => {
+  return (
+    <p className="font-title font-black">
+      he
+      <span className="text-red">▶︎</span>
+      ya
+    </p>
+  );
+};
+
+export default Logo;
