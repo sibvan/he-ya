7fa7b9a949f7ed45ee01a6d4950cb85137148cc5
sibvan <sibvan@icloud.com>
2026-02-13 16:14:22 +0400

    Add updates page and link in footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,4 +1,5 @@
 import Logo from "../ui/Logo";
+import Link from "next/link";
 
 const Footer = () => {
   return (
@@ -6,19 +7,26 @@ const Footer = () => {
       <hr className="text-black" />
       <div className="mt-4 flex justify-between">
         <div className="font-title flex text-[16px]/[100%]">
-          © <Logo />, 2025
+          © 
+          <Logo />
+          , 2026
         </div>
 
-        <p className="text-16">
-          Created by 
-          <a
-            className="decoration-red underline"
-            href="https://sibvan.dev/"
-            target="_blank"
-          >
-            sibvan.dev
-          </a>
-        </p>
+        <div className="flex gap-6">
+          <Link className="text-16" href="/updates">
+            Обновления
+          </Link>
+          <p className="text-16">
+            <span className="hidden md:inline">Created by </span>
+            <a
+              className="decoration-red underline"
+              href="https://sibvan.dev/"
+              target="_blank"
+            >
+              sibvan.dev
+            </a>
+          </p>
+        </div>
       </div>
     </footer>
   );
